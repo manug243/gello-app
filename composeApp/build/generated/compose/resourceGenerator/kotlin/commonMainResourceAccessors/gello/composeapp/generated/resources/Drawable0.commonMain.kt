a72f1b47f2e549c70445fc48deab1a3aea7e9273
Manuel Gilles <117361287+manug243@users.noreply.github.com>
2026-01-27 11:31:14 +0100

    fix: changed theme to fit placeholder image

--- composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/gello/composeapp/generated/resources/Drawable0.commonMain.kt
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/gello/composeapp/generated/resources/Drawable0.commonMain.kt
@@ -3,13 +3,28 @@
 package gello.composeapp.generated.resources
 
 import kotlin.OptIn
-import org.jetbrains.compose.resources.InternalResourceApi
+import kotlin.String
+import kotlin.collections.MutableMap
 import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.InternalResourceApi
 import org.jetbrains.compose.resources.ResourceItem
-import org.jetbrains.compose.resources.LanguageQualifier
-import org.jetbrains.compose.resources.RegionQualifier
-import org.jetbrains.compose.resources.ThemeQualifier
-import org.jetbrains.compose.resources.DensityQualifier
 
-internal val Res.drawable.appicon: DrawableResource by lazy { DrawableResource("drawable:appicon", setOf(ResourceItem(setOf(),"drawable/appicon.png",-1,-1))) }
-internal val Res.drawable.compose_multiplatform: DrawableResource by lazy { DrawableResource("drawable:compose_multiplatform", setOf(ResourceItem(setOf(),"drawable/compose-multiplatform.xml",-1,-1))) }
+private const val MD: String = "composeResources/gello.composeapp.generated.resources/"
+
+internal val Res.drawable.appicon: DrawableResource by lazy {
+      DrawableResource("drawable:appicon", setOf(
+        ResourceItem(setOf(), "${MD}drawable/appicon.png", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.compose_multiplatform: DrawableResource by lazy {
+      DrawableResource("drawable:compose_multiplatform", setOf(
+        ResourceItem(setOf(), "${MD}drawable/compose-multiplatform.xml", -1, -1),
+      ))
+    }
+
+@InternalResourceApi
+internal fun _collectCommonMainDrawable0Resources(map: MutableMap<String, DrawableResource>) {
+  map.put("appicon", Res.drawable.appicon)
+  map.put("compose_multiplatform", Res.drawable.compose_multiplatform)
+}
