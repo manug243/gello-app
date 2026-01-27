06926b5fa97f0c943d618b5488b818c70540e5c0
Manuel Gilles <117361287+manug243@users.noreply.github.com>
2026-01-27 11:45:13 +0100

    feat: added component for topapp bar with navigation back

---
+++ core/designsystem/src/commonMain/kotlin/de/gello/designsystem/component/TopAppBarWithBackNavigation.kt
@@ -0,0 +1,33 @@
+package de.gello.designsystem.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarWithBackNavigation(
+    title: String,
+    onNavigateBack: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(text = title)
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = onNavigateBack
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
